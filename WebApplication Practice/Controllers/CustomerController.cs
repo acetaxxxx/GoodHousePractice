876f5280a4c1d8e8778f7c3b5b49beee27ab225f
acetaxxxx <acetaxxxx@gmail.com>
2018-04-30 18:36:56 +0800

    建立新的Controller以及畫面

---
+++ WebApplication Practice/Controllers/CustomerController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication_Practice.Controllers
+{
+	public class CustomerController : Controller
+	{
+		public ActionResult Customer()
+		{
+			return View();
+		}
+	}
+}
