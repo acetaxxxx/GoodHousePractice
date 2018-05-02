756d8f0b1434bd564c233570b4552dd0a6e10aaf
acetaxxxx <acetaxxxx@gmail.com>
2018-05-02 11:05:29 +0800

    To create a grid view of Customer query result. Create data for the gridvew Create the GridViewModel

--- WebApplication Practice/Controllers/CustomerController.cs
+++ WebApplication Practice/Controllers/CustomerController.cs
@@ -3,14 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication_Practice.ViewModels.Customer;
 
 namespace WebApplication_Practice.Controllers
 {
 	public class CustomerController : Controller
 	{
+		/// <summary>
+		/// Customer首頁
+		/// </summary>
+		/// <returns></returns>
 		public ActionResult Customer()
 		{
 			return View();
+		}
+
+		/// <summary>
+		/// 查詢
+		/// </summary>
+		/// <returns></returns>
+		public ActionResult QueryGrid()
+		{
+			var list = new List<CustomerGridViewModel>()
+			{
+				new CustomerGridViewModel()
+				{
+					CompanyName = "First Company",
+					City = "Taipei",
+					Country = "Taipei",
+					CustomerName = "Stark Shark",
+					CustomerTitle = "Vice President",
+					PhoneNumber = "0123456789"
+				},
+				new CustomerGridViewModel()
+				{
+					CompanyName = "Second Company",
+					City = "Hsinchu",
+					Country = "Taipei",
+					CustomerName = "Stark Shark",
+					CustomerTitle = " President",
+					PhoneNumber = "+886 351234567"
+				},
+			};
+			return PartialView("_Grid" , list);
 		}
 	}
 }
